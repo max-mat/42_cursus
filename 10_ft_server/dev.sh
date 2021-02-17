f493d839a1d5d3f4e7475a59f0396b4d58aaa581
Maxim Matsegora <mmatsego@e4r8p2.42roma.it>
2021-02-17 18:46:17 +0100

    ft_server

--- 10_ft_server/dev.sh
+++
@@ -1,3 +0,0 @@
-docker container prune -f
-docker build -t try .
-docker run --name prova -it -p 80:80 -p 443:443 try
--- 10_ft_server/srcs/start.sh
+++ 10_ft_server/srcs/start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 service nginx start
+service php7.3-fpm start
 
+# Configure a wordpress database
+service mysql start
+echo "CREATE DATABASE wordpress;"| mysql -u root --skip-password
+echo "CREATE USER 'admin'@localhost IDENTIFIED BY 'admin';" | mysql -u root --skip-password
+echo "GRANT ALL PRIVILEGES ON wordpress.* TO 'admin'@'localhost' WITH GRANT OPTION;"| mysql -u root --skip-password
+echo "FLUSH PRIVILEGES;"| mysql -u root --skip-password
+echo "update mysql.user set plugin='mysql_native_password' where user='admin';" | mysql -u root --skip-password
 
 bash
